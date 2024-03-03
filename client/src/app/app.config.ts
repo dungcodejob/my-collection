57acccf7179e43676c63828ee648b6d8c28ff2ca
Pham Van Anh Dung <dungcodejob@gmail.com>
2024-03-03 20:11:25 +0700

    chore(client/config): setting config using json file

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from "@angular/core";
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
+import { provideCore } from "./core";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [provideCore(), provideRouter(routes)],
 };
