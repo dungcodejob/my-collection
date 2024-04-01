506177a2b3bd1a2dac79929e7e9d708332d187aa
Pham Van Anh Dung <dungcodejob@gmail.com>
2024-04-02 00:39:18 +0700

    chore(server/auth): apply nest/passport package

--- server/src/authentication/auth.module.ts
+++ server/src/authentication/auth.module.ts
@@ -4,10 +4,16 @@ import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from '@modules/user';
 import { JwtUtil } from './services';
 import { BcryptService } from './services/bcrypt.service';
+import { AccessTokenStrategy, RefreshTokenStrategy } from './strategies';
 
 @Module({
   imports: [JwtModule, UserModule],
-  providers: [JwtUtil, BcryptService],
+  providers: [
+    JwtUtil,
+    BcryptService,
+    AccessTokenStrategy,
+    RefreshTokenStrategy,
+  ],
   exports: [JwtUtil, BcryptService],
 })
 export class AuthModule {}
