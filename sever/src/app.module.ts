0900c3da32b122cd7465d0e4ef41b4a324526b9e
dungcodejob <dungcodejob@gmail.com>
2025-08-24 23:33:56 +0700

    feat(config): add ConfigModule with app and cookie configs
    Initialize NestJS ConfigModule with app and cookie configurations, using environment-specific .env files and making it globally available

--- sever/src/app.module.ts
+++ sever/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { appConfig, cookieConfig } from './configs';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [appConfig, cookieConfig],
+      envFilePath: `./.env.${process.env.NODE_ENV || 'dev'}`,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
